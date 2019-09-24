fe5ef193afc5af98e608314e680653e0ad48f73a
Ho Nhat Anh <33145672+nhatanh28592@users.noreply.github.com>
2019-09-24 09:17:12 +0700

    Add files via upload
    Add project from local to githup

---
+++ nhatanhdemo/src/main/java/nhatanh/example/nhatanhdemo/service/TodoService.java
@@ -0,0 +1,16 @@
+package nhatanh.example.nhatanhdemo.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import nhatanh.example.nhatanhdemo.model.Todo;
+
+@Repository
+public interface TodoService extends JpaRepository<Todo, Long>{
+	@Query(value = "SELECT * FROM todo WHERE work_name = ?1 ", nativeQuery = true)
+    Todo findByWorkName(String workName);
+	
+	@Query(value = "SELECT * FROM todo WHERE id = ?1 ", nativeQuery = true)
+    Todo findById(long id);
+}
